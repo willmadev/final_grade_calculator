eb1d3bea625f7c808137e3b034f6cb74a95092fb
William Ma <65773478+willmadev@users.noreply.github.com>
2024-05-02 00:25:41 -0400

    Refactor to auth sessions

--- server/src/api/auth/auth.controller.ts
+++ server/src/api/auth/auth.controller.ts
@@ -1,27 +1,14 @@
-import { CookieOptions, Request, Response } from "express";
+import { Request, Response } from "express";
 import { ZodError, z } from "zod";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-
 import prisma from "../../config/prisma";
-import { config } from "../../config/env";
+import { User } from "@prisma/client";
 
 const saltRounds = 10;
 
-const generateTokens = (userId: number) => {
-  const accessToken = jwt.sign({ userId }, config.accessTokenSecret, {
-    expiresIn: "1 hour",
-  });
-  const refreshToken = jwt.sign({ userId }, config.refreshTokenSecret, {
-    expiresIn: "1 week",
-  });
-  return { accessToken, refreshToken };
-};
-
-const refreshTokenOptions: CookieOptions = {
-  httpOnly: true,
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-  path: "/api/auth/refresh-tokens",
+const createSession = async (user: User) => {
+  const session = await prisma.session.create({ data: { userId: user.id } });
+  return session.id;
 };
 
 const registerSchema = z.object({
@@ -52,10 +39,8 @@ export const register = async (req: Request, res: Response) => {
     data: { email: reqBody.email, password: hash },
   });
 
-  const { accessToken, refreshToken } = generateTokens(user.id);
-  res.cookie("refreshToken", refreshToken, refreshTokenOptions);
-
-  return res.status(200).send({ accessToken });
+  res.cookie("session", await createSession(user));
+  return res.status(200).send();
 };
 
 const loginSchema = z.object({
@@ -84,26 +69,9 @@ export const login = async (req: Request, res: Response) => {
     return res.status(400).send("Incorrect password");
   }
 
-  const { accessToken, refreshToken } = generateTokens(user.id);
-  res.cookie("refreshToken", refreshToken, refreshTokenOptions);
-  return res.status(200).send({ accessToken });
+  res.cookie("session", await createSession(user));
+  return res.status(200).send();
 };
 
-export const refreshTokens = (req: Request, res: Response) => {
-  if (!req.cookies || !req.cookies.refreshToken) {
-    return res.status(401).send("Invalid refresh token");
-  }
-  const jwtPayload = jwt.verify(
-    req.cookies.refreshToken,
-    config.refreshTokenSecret
-  );
-  if (!jwtPayload || typeof jwtPayload === "string" || !jwtPayload.userId) {
-    return res.status(401).send("Invalid refresh token");
-  }
-  const { accessToken, refreshToken } = generateTokens(jwtPayload.userId);
-  res.cookie("refreshToken", refreshToken, refreshTokenOptions);
-  return res.status(200).send({ accessToken });
-};
-
-export const resetPassword = (req: Request, res: Response) => {};
-export const changeEmail = (req: Request, res: Response) => {};
+export const resetPassword = async (req: Request, res: Response) => {};
+export const changeEmail = async (req: Request, res: Response) => {};
--- server/src/api/auth/auth.router.ts
+++ server/src/api/auth/auth.router.ts
@@ -1,16 +1,9 @@
 import { Router } from "express";
-import {
-  changeEmail,
-  login,
-  refreshTokens,
-  register,
-  resetPassword,
-} from "./auth.controller";
+import { changeEmail, login, register, resetPassword } from "./auth.controller";
 
 const authRouter = Router();
 authRouter.post("/register", register);
 authRouter.post("/login", login);
-authRouter.post("/refresh-tokens", refreshTokens);
 // authRouter.post("/reset-password", resetPassword);
 // authRouter.post("/change-email", changeEmail);
 
